daab9176363a7c31174cf146daa70fc0e382f547
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-05-22 13:41:52 -0400

    Typed greeting loop in main class

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -18,4 +18,35 @@ public class MorseCode {
                     "7", "8", "9", "0"};
 
 
+    static Scanner keyboard = new Scanner(System.in);
+    static boolean input = true;
+
+    public static void main(String[] args){
+
+        String codeTranslation;
+
+        while(input){
+                System.out.print("This is a morse code and english translator, type \"morse\" to do morse code" +
+                        "to English or type \"English\" to do English to morse code");
+                codeTranslation= (keyboard.nextln());
+    }
+    keyboard.close();
+        System.out.println("Thank you for using the morse code and English translator");
+        System.exit(0);
+
+public static String morseToAlphabet (String[]codeTranslation){
+
+        if (codeTranslation = \"morse")
+
+
+
+
+}
+
+
+
+
+
+
+
 }
