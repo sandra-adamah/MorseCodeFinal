dcb30e33c4ef542cccc0c7ffe3eed7f6a68a8f0d
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-05-22 13:06:15 -0400

    Created morse code class and translations

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,21 @@
 package com.LickingHeights;
 
-public class Main {
+public class MorseCode {
+    public static final String[] morseCode =
+            {"*-", "-***", "-*-*", "-**", "*", "**-*", "--*",
 
-    public static void main(String[] args) {
-	sdgsfg
-    }
+                    "****", "**", "*---", "-*-*", "*-**", "--", "-*", "---",
+
+                    "*--*", "--*-", "*-*", "***", "-", "**-", "***-", "*--",
+
+                    "-**-", "-*--", "--**", "*----", "**---", "***--", "****-",
+
+                    "*****", "-****", "--***", "---**", "----*", "-----"};
+    public static final String[] alphabet =
+            {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+                    "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6",
+                    "7", "8", "9", "0"};
+
+
 }
